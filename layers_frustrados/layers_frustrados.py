412919277531dbbfb38c7c991ea33fb547707fdb
Fernando Michelotti <artista.frustrado@gmail.com>
2021-05-22 10:55:17 -0300

    added plugin registering routines

--- layers_frustrados/layers_frustrados.py
+++ layers_frustrados/layers_frustrados.py
@@ -1,7 +1,28 @@
-import krita
+#import krita
+from krita import *
 #from krita import *
 
 #Krita.instance().setBatchmode(True)
+
+class LayersFrustrados(Extension):
+
+    def __init__(self, parent):
+        # This is initialising the parent, always important when subclassing.
+        super().__init__(parent)
+
+    def setup(self):
+        pass
+
+    def createActions(self, window):
+        action = window.createAction("layers_frustrados", "Layers Frustrados", "tools/scripts")
+        action.triggered.connect(self.trigger_layers_frustrados)
+
+    def trigger_layers_frustrados(self):
+        trigger_layers_frustrados()
+
+# And add the extension to Krita's list of extensions:
+Krita.instance().addExtension(LayersFrustrados(Krita.instance()))
+
 
 def trigger_layers_frustrados():
 
@@ -56,30 +77,30 @@ def trigger_layers_frustrados():
     #info = krita.InfoObject()
     #info.setProperty("pattern", "Cross01.pat")
     #selection = krita.Selection();
-#selection.select(0, 0, doc.width(), doc.height(), 255)
-#node = doc.createFillLayer("Pattern", "pattern", info, selection)
-#linework.addChildNode(node, None)
-#doc.refreshProjection()
+        #selection.select(0, 0, doc.width(), doc.height(), 255)
+    #node = doc.createFillLayer("Pattern", "pattern", info, selection)
+    #linework.addChildNode(node, None)
+    #doc.refreshProjection()
 
-# color
+    # color
 
-#info = krita.InfoObject()
-#info.setProperty("color","transparent")
-##info.setProperty("color",(0, 0, 0, 0))
-#selection = krita.Selection();
-#selection.select(0, 0, doc.width(), doc.height(), 255)
-#node = doc.createFillLayer("lineart", "color", info, selection)
-#linework.addChildNode(node, None)
-#doc.refreshProjection()
+    #info = krita.InfoObject()
+    #info.setProperty("color","transparent")
+    ##info.setProperty("color",(0, 0, 0, 0))
+    #selection = krita.Selection();
+    #selection.select(0, 0, doc.width(), doc.height(), 255)
+    #node = doc.createFillLayer("lineart", "color", info, selection)
+    #linework.addChildNode(node, None)
+    #doc.refreshProjection()
 
 
-# add_new_paint_layer
-# convert_to_paint_layer
+    # add_new_paint_layer
+    # convert_to_paint_layer
 
-# Document().CreateNode or Krita.instance().openDocument(“tmp/rcpg.svg”)
+    # Document().CreateNode or Krita.instance().openDocument(“tmp/rcpg.svg”)
 
-#r = doc.rootNode()
-#t = doc.createNode("Transparency Mask", "transparencymask")
-#c = r.childNodes()[0] # I'm always using the "first layer" here
-#c.addChildNode(t, None)
-#doc.refreshProjection()
+    #r = doc.rootNode()
+    #t = doc.createNode("Transparency Mask", "transparencymask")
+    #c = r.childNodes()[0] # I'm always using the "first layer" here
+    #c.addChildNode(t, None)
+    #doc.refreshProjection()
